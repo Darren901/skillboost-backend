42ff82a561c17424be504276c38ee8e7fd55d9c3
Darren <q0981516608@gmail.com>
2024-12-11 22:51:07 +0800

    fixed cors

--- index.js
+++ index.js
@@ -18,7 +18,15 @@ mongoose
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000", // 開發環境
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
 
 app.use("/api/ecpay", ecpay);
 app.use("/api/user", authRoutes);
